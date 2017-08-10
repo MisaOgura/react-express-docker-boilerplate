3321230d71cc614a68a5a1177904429fa5d2cbd3
MisaOgura <misa.ogura01@gmail.com>
2017-08-10 20:01:53 +0100

    Setup babel and webpack for bundling react frontend

---
+++ src/client/components/app.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function App () {
+  return <div>React is up and running!</div>
+}
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: ['./src/client'],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/
+
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/client/index.html'),
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+}
