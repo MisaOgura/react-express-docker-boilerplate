47ae5abb18317bd2fb4d0635e3ba0f5938ccd7e1
MisaOgura <misa.ogura01@gmail.com>
2017-09-20 17:58:57 +0100

    Use async/await

--- app/server/__tests__/server.test.js
+++ app/server/__tests__/server.test.js
@@ -3,12 +3,10 @@ import request from 'supertest'
 import server from '../server'
 
 describe('Server', () => {
-  it('renders HelloWorld component', () => {
-    return request(server)
-      .get('/')
-      .expect(200)
-      .then(res => {
-        expect(res.text).toContain('class="hello-world"')
-      })
+  it('renders HelloWorld component', async () => {
+    const res = await request(server).get('/')
+
+    expect(res.statusCode).toBe(200)
+    expect(res.text).toContain('class="hello-world"')
   })
 })
