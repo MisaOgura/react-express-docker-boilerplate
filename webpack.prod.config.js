82f9f55479060fbdba5313faea95162768b059a1
MisaOgura <misa.ogura01@gmail.com>
2017-10-10 18:13:31 +0100

    Add comments for better instructions

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,7 @@
 const config = require('./webpack.config.js')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
+// Reduces a bundle size in production by retrieving those dependencies externally at runtime.
 config.externals = {
   'react': 'React',
   'react-dom': 'ReactDOM'
